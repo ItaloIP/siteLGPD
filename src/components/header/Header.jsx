c90777d8407de086a62212a3b432e43fea40fee5
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2023-07-14 17:41:16 -0300

    Novo background

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,7 +18,7 @@ function Header(){
                 <div className="flex flex-row cursor-pointer" onClick={changeName}>
     
                     <img src="https://i0.wp.com/omeudiadia.com.br/wp-content/uploads/2022/04/gatito.webp?resize=700%2C592&amp;ssl=1" alt="" width="50px" height="40px"/>
-                    <div className="flex items-center mx-2.5" id="Username">{nome}</div>
+                    <div className="flex items-center mx-2.5 text-white font-normal " id="Username">{nome}</div>
                 </div>
                 <input type="button" value="Português/BR" className="bg-neutral-950 rounded text-white border-white border-solid border-2 p-1 cursor-pointer transform transition duration-300 hover:scale-110 hidden" onClick={buttonDark}/>
                 <input type="button" value="English/USA" className="bg-neutral-950 rounded text-white border-white border-solid border-2 p-1 cursor-pointer transform transition duration-300 hover:scale-110 " onClick={buttonDark}/>
